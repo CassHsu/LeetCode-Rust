f3c32105cebfd279bd53eb29ecbdb37b06e9b816
CassHsu <cass.hsu@gmail.com>
2023-01-18 23:14:57 +0800

    Update 2529-01.rs

--- 2529/2529-01.rs
+++ 2529/2529-01.rs
@@ -4,8 +4,14 @@ impl Solution {
         let mut n = 0;
 
         for num in nums {
-            if (num > 0) { p += 1 };
-            if (num < 0) { n += 1 };
+            if (num > 0) { 
+                p += 1;
+                continue
+            };
+            if (num < 0) {
+                n += 1;
+                continue
+            };
         }
 
         if (p > n) {
